6fcfe1300c1e4ae24021c6ad66c83e6614d5e703
wiktor <wikcioo@gmail.com>
2021-12-07 14:01:29 +0100

    Created abstract Person class that Student and Teacher extends

---
+++ JavaFXSchedulingApplication/src/model/students/Person.java
@@ -0,0 +1,20 @@
+package model.students;
+
+public abstract class Person {
+    protected String name;
+    protected String email;
+    protected int id;
+
+    public Person(String name, String email, int id) {
+        this.name = name;
+        this.email = email;
+        this.id = id;
+    }
+
+    public abstract String getName();
+    public abstract void setName(String name);
+    public abstract String getEmail();
+    public abstract void setEmail(String email);
+    public abstract int getId();
+    public abstract void setId(int id);
+}
