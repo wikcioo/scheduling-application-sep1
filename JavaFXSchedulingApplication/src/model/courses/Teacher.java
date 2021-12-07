56571e6e6e3c525cb5dfa1c67b6e423283a70af2
wiktor <wikcioo@gmail.com>
2021-12-07 14:02:42 +0100

    Modified Student And Teacher class to extend Person abstract class

--- JavaFXSchedulingApplication/src/model/courses/Teacher.java
+++ JavaFXSchedulingApplication/src/model/courses/Teacher.java
@@ -1,16 +1,35 @@
 package model.courses;
 
+import model.students.Person;
+
 import java.io.Serializable;
 
-public class Teacher implements Serializable
-{
-    private String name;
-
-    public Teacher(String name){
-        this.name = name;
+public class Teacher extends Person implements Serializable {
+    public Teacher(String name) {
+        super(name, "", 0);
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
--- JavaFXSchedulingApplication/src/model/students/Student.java
+++ JavaFXSchedulingApplication/src/model/students/Student.java
@@ -3,80 +3,90 @@ package model.students;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class Student implements Serializable {
-    private int id;
+public class Student extends Person implements Serializable {
     private int semester;
     private String _class;
-    private String name;
     private boolean isExchange;
 
     public Student(String name, int id, String _class, int semester) {
-        this.name = name;
-        this.id = id;
+        super(name, id + "@via.dk", id);
         this._class = _class;
         this.semester = semester;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setSemester(int semester) {
-        this.semester = semester;
-    }
-
-    public void set_class(String _class) {
+    public Student(String name, String email, int id, String _class, int semester) {
+        super(name, email, id);
         this._class = _class;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setExchange(boolean exchange) {
-        isExchange = exchange;
-    }
-
-    public int getId() {
-        return id;
+        this.semester = semester;
     }
 
     public int getSemester() {
         return semester;
     }
 
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
     public String get_class() {
         return _class;
     }
 
-    public String getName() {
-        return name;
+    public void set_class(String _class) {
+        this._class = _class;
     }
 
     public boolean isExchange() {
         return isExchange;
     }
 
-    @Override
+    public void setExchange(boolean exchange) {
+        isExchange = exchange;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Student student = (Student) o;
-        return id == student.id && semester == student.semester && Objects.equals(_class, student._class) && Objects.equals(name, student.name);
+        return getId() == student.getId() && semester == student.semester && Objects.equals(_class, student._class) && Objects.equals(getName(), student.getName());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, semester, _class, name);
+        return Objects.hash(getId(), semester, _class, getName());
     }
 
     @Override
     public String toString() {
         return "Student{" +
-            "id=" + id +
+            "id=" + getId() +
             ", semester=" + semester +
             ", _class='" + _class + '\'' +
-            ", name='" + name + '\'' +
+            ", name='" + getName() + '\'' +
             '}';
     }
 }
