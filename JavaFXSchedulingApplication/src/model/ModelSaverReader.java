41434abd821740eed9e2cf8bcc9f093e8462a5b0
wiktor <wikcioo@gmail.com>
2021-12-10 13:27:09 +0100

    Created a new class for saving and reading model data from a binary file

---
+++ JavaFXSchedulingApplication/src/model/ModelSaverReader.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.io.*;
+
+public class ModelSaverReader {
+    private static final String filename = "res/saved-data/models.bin";
+
+    public static void saveModel(Model model) {
+        ObjectOutputStream out = null;
+        try {
+            File file = new File(filename);
+            FileOutputStream fos = new FileOutputStream(file);
+            out = new ObjectOutputStream(fos);
+            out.writeObject(model);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Exception: " + filename);
+        } finally {
+            try {
+                out.close();
+            } catch (NullPointerException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static Model readModel() {
+        ObjectInputStream in = null;
+        try {
+            File file = new File(filename);
+            FileInputStream fis = new FileInputStream(file);
+            in = new ObjectInputStream(fis);
+            try {
+                Model m = (Model) in.readObject();
+                return m;
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
