beafdd95b62f39a9b0fdcb8623c68d4e1a37a4aa
wiktor <wikcioo@gmail.com>
2021-12-10 17:31:44 +0100

    Overloaded getMonday method to accept either no or one parameter of type LocalDate

--- JavaFXSchedulingApplication/src/utilities/Util.java
+++ JavaFXSchedulingApplication/src/utilities/Util.java
@@ -4,12 +4,46 @@ import java.time.LocalDate;
 
 public class Util {
     public static LocalDate getMonday() {
-        LocalDate now = LocalDate.now();
+        return Util.getMonday(LocalDate.now());
+    }
+
+    public static LocalDate getMonday(LocalDate current) {
         LocalDate monday;
-        if (now.getDayOfWeek().getValue() > 1) {
-            int daysSubtracted = now.getDayOfWeek().getValue() - 1;
-            monday = now.minusDays(daysSubtracted);
-        } else monday = now;
+        if (current.getDayOfWeek().getValue() > 1) {
+            int daysSubtracted = current.getDayOfWeek().getValue() - 1;
+            monday = current.minusDays(daysSubtracted);
+        } else monday = current;
         return monday;
+    }
+
+    public static int monthStringToMonthInt(String month) {
+        switch (month.toLowerCase()) {
+            case "january":
+                return 1;
+            case "february":
+                return 2;
+            case "march":
+                return 3;
+            case "april":
+                return 4;
+            case "may":
+                return 5;
+            case "june":
+                return 6;
+            case "july":
+                return 7;
+            case "august":
+                return 8;
+            case "september":
+                return 9;
+            case "october":
+                return 10;
+            case "november":
+                return 11;
+            case "december":
+                return 12;
+        }
+
+        return 0;
     }
 }
