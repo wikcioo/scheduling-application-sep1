0356f048d0999031fad8015f393ebdefec55c0cc
wiktor <wikcioo@gmail.com>
2021-12-13 10:57:58 +0100

    Added pop-ups for Save and Load buttons

--- JavaFXSchedulingApplication/src/controller/MainMenuController.java
+++ JavaFXSchedulingApplication/src/controller/MainMenuController.java
@@ -1,10 +1,15 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.Region;
 import model.Model;
 import model.ModelSaverReader;
+import model.rooms.Room;
 import view.ViewHandler;
+
+import java.util.Optional;
 
 public class MainMenuController extends ViewController {
     private Region root;
@@ -38,13 +43,29 @@ public class MainMenuController extends ViewController {
 
     @FXML
     public void onSaveButtonClick() {
-        ModelSaverReader.saveModel(this.model);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Saving current state of the system");
+        alert.setHeaderText("Confirm to save the current system");
+        alert.setContentText("Are you sure you want to save the current system state");
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            ModelSaverReader.saveModel(this.model);
+        }
     }
 
     @FXML
     public void onLoadButtonClick() {
         this.model = ModelSaverReader.readModel();
-        if (this.model != null)
+        if (this.model != null) {
             this.viewHandler.reassignModel(this.model);
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Successfully loaded current system state", ButtonType.OK);
+            alert.setHeaderText("Information");
+            alert.show();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Couldn't load current system state");
+            alert.show();
+        }
     }
 }
