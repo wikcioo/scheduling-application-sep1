45a20ac75c7e957644a40797545b4f6c22cfa8b2
wiktor <wikcioo@gmail.com>
2021-12-14 15:50:08 +0100

    Added sortLessons() analysis

--- JavaFXSchedulingApplication/src/model/calendar/Day.java
+++ JavaFXSchedulingApplication/src/model/calendar/Day.java
@@ -42,24 +42,45 @@ public class Day implements Serializable {
         sortLessons();
     }
 
+    /**
+     *  This method sorts lesson objects according to start time in ascending order
+     */
     private void sortLessons() {
-        for (int i = 0; i < lessons.size() - 1; i++) {
-            boolean wasChanged = false;
-            for (int j = 0; j < lessons.size() - i - 1; j++) {
-                if (lessons.get(j).getStart().isAfter(lessons.get(j + 1).getStart())) {
-                    swapElements(j, j + 1);
-                    wasChanged = true;
+        // iterate through the entire lessons ArrayList except the last element
+        for (int i = 0; i < lessons.size() - 1; i++) { // This loop will run n times
+
+            // We initialize 'wasChanged' variable to false
+            boolean wasChanged = false; // This takes 2 time units
+
+            // for each iteration loop again through the entire ArrayList minus one minus i
+            for (int j = 0; j < lessons.size() - i - 1; j++) { // For each iteration it will run n times
+
+                // Check if one lesson is after another
+                if (lessons.get(j).getStart().isAfter(lessons.get(j + 1).getStart())) { // This takes 5 time units
+                    // Call to swapElements
+                    swapElements(j, j + 1); // This takes 6 time units
+
+                    // If elements were swapped, then we assign true to 'wasChanged' variable
+                    wasChanged = true; // This takes 1 time unit
                 }
             }
 
-            if (!wasChanged) break;
+            // We break from the for loop, thus returning from the method
+            if (!wasChanged) { // This takes 1 time unit
+                break; // This takes 1 time unit
+            }
         }
+
+        // We have no recursion, so we do not need a base case
+        // T(n) = n * (12n + 4)
+        // T(n) = O(n^2) - worst case scenario
+        // T(n) = O(n)   - best case scenario
     }
 
-    private void swapElements(int a, int b) {
-        Lesson temp = lessons.get(a);
-        lessons.set(a, lessons.get(b));
-        lessons.set(b, temp);
+    private void swapElements(int a, int b) { // 6 time units
+        Lesson temp = lessons.get(a); // 3 time units
+        lessons.set(a, lessons.get(b)); // 2 time units
+        lessons.set(b, temp); // 1 time unit
     }
 
     public void removeLesson(Lesson lesson) {
