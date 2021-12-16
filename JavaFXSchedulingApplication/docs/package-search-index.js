cb3f3843d76e88bc4979b35a43098b6eb8929cda
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2021-12-16 22:10:54 +0100

    Added updated JavaDoc

--- JavaFXSchedulingApplication/docs/package-search-index.js
+++ JavaFXSchedulingApplication/docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"model"},{"l":"model.calendar"},{"l":"model.courses"},{"l":"model.rooms"},{"l":"model.students"},{"l":"Test"},{"l":"utilities"},{"l":"view"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"controller"},{"l":"model"},{"l":"model.calendar"},{"l":"model.courses"},{"l":"model.rooms"},{"l":"model.students"},{"l":"Test"},{"l":"utilities"},{"l":"view"}]
