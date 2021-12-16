286b7e60f334fdd5783d1696eb8fa85a6800d757
wiktor <wikcioo@gmail.com>
2021-12-16 09:57:48 +0100

    Generated Javadoc in docs folder

---
+++ JavaFXSchedulingApplication/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"model"},{"l":"model.calendar"},{"l":"model.courses"},{"l":"model.rooms"},{"l":"model.students"},{"l":"Test"},{"l":"utilities"},{"l":"view"}];updateSearchResults();
